44080f23f30110e0aeb3b3e2084238620538ffbf
lorand-ffxi <lorand.ffxi@gmail.com>
2015-03-07 12:03:24 -0500

    added functionality

--- spellSpammer/spellSpammer.lua
+++ spellSpammer/spellSpammer.lua
@@ -1,28 +1,52 @@
 _addon.name = 'spellSpammer'
 _addon.author = 'Lorand'
-_addon.command = 'spellSpammer'
-_addon.version = '1.0'
+_addon.commands = {'spam','spellSpammer'}
+_addon.version = '1.1'
 
-res = require('resources')
-spellToSpam = 'Stone'
-keepSpamming = false
-spamDelay = 0.8
+local res = require('resources')
+local config = require('config')
+local aliases = config.load('..\\shortcuts\\data\\aliases.xml')
+local spellToSpam = 'Stone'
+local keepSpamming = false
+local spamDelay = 0.8
 
 windower.register_event('addon command', function (command,...)
     command = command and command:lower() or 'help'
     local args = {...}
 	
 	if command == 'reload' then
-		windower.send_command('lua unload spellSpammer; lua load spellSpammer')
+		windower.send_command('lua reload spellSpammer')
 	elseif command == 'unload' then
 		windower.send_command('lua unload spellSpammer')
+	elseif S{'on','start'}:contains(command) then
+		keepSpamming = true
+		print_status()
+	elseif S{'off','stop'}:contains(command) then
+		keepSpamming = false
+		print_status()
 	elseif command == 'toggle' then
 		keepSpamming = not keepSpamming
 		print_status()
+	elseif S{'use','cast'}:contains(command) then
+		local arg_string = table.concat(args,' ')
+		local spellName = formatSpellName(arg_string)
+		local spell = res.spells:with('en', spellName)
+		if (spell ~= nil) then
+			if canCast(spell) then
+				spellToSpam = spell.en
+				atc(0,'Successfully changed spell to spam to: '..spell.en)
+			else
+				atc(123,'Error: Unable to cast '..spell.en)
+			end
+		else
+			atc(123,'Error: Invalid spell name: '..arg_string..' | '..spellName)
+		end
+		
+		
 	elseif command == 'status' then
 		print_status()
 	else
-		windower.add_to_chat(0, 'Error: Unknown command')
+		atc(123, 'Error: Unknown command')
 	end
 end)
 
@@ -42,7 +66,11 @@ windower.register_event('prerender', function()
 				if (windower.ffxi.get_spell_recasts()[spell.recast_id] == 0) then
 					if (player.vitals.mp >= spell.mp_cost) and (mob.hpp > 0) then
 						windower.send_command('input '..spell.prefix..' "'..spell.en..'" <t>')
-						spamDelay = spell.recast + 1.6 + (math.random(2, 9)/10)
+						local add_delay = 1.6
+						if (spell.recast >= 4) then
+							add_delay = 0.2
+						end
+						spamDelay = spell.recast + add_delay + (math.random(2, 9)/10)
 					end
 				end
 			end
@@ -54,6 +82,82 @@ end)
 function print_status()
 	local onoff = keepSpamming and 'On' or 'Off'
 	windower.add_to_chat(0, '[spellSpammer: '..onoff..'] {'..spellToSpam..'}')
+end
+
+function atc(c, msg)
+	if (type(c) == 'string') and (msg == nil) then
+		msg = c
+		c = 0
+	end
+	windower.add_to_chat(c, '[spellSpammer]'..msg)
+end
+
+function canCast(spell)
+	if spell.prefix == '/magic' then
+		local player = windower.ffxi.get_player()
+		if (player == nil) or (spell == nil) then return false end
+		local mainCanCast = (spell.levels[player.main_job_id] ~= nil) and (spell.levels[player.main_job_id] <= player.main_job_level)
+		local subCanCast = (spell.levels[player.sub_job_id] ~= nil) and (spell.levels[player.sub_job_id] <= player.sub_job_level)
+		local spellAvailable = windower.ffxi.get_spells()[spell.id]
+		return spellAvailable and (mainCanCast or subCanCast)
+	end
+	return true
+end
+
+dec2roman = {'I','II','III','IV','V','VI','VII','VIII','IX','X','XI'}
+roman2dec = {['I']=1,['II']=2,['III']=3,['IV']=4,['V']=5,['VI']=6,['VII']=7,['VIII']=8,['IX']=9,['X']=10,['XI']=11}
+
+function formatSpellName(text)
+	if (type(text) ~= 'string') or (#text < 1) then return nil end
+	
+	if (aliases ~= nil) then
+		local fromAlias = aliases[text]
+		if (fromAlias ~= nil) then
+			return fromAlias
+		end
+	end
+	
+	local parts = text:split(' ')
+	if #parts >= 2 then
+		local name = formatName(parts[1])
+		for p = 2, #parts do
+			local part = parts[p]
+			local tier = toRomanNumeral(part) or part:upper()
+			if (roman2dec[tier] == nil) then
+				name = name..' '..formatName(part)
+			else
+				name = name..' '..tier
+			end
+		end
+		return name
+	else
+		local name = formatName(text)
+		local tier = text:sub(-1)
+		local rnTier = toRomanNumeral(tier)
+		if (rnTier ~= nil) then
+			return name:sub(1, #name-1)..' '..rnTier
+		else
+			return name
+		end
+	end
+end
+
+function formatName(text)
+	if (text ~= nil) and (type(text) == 'string') then
+		return text:lower():ucfirst()
+	end
+	return text
+end
+
+function toRomanNumeral(val)
+	if type(val) ~= 'number' then
+		if type(val) == 'string' then
+			val = tonumber(val)
+		else
+			return nil
+		end
+	end
+	return dec2roman[val]
 end
 
 -----------------------------------------------------------------------------------------------------------
