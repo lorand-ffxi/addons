9bc197486883c70ac5d420c2a35d6fc85c9867c4
lorand-ffxi <lorand.ffxi@gmail.com>
2015-05-01 22:53:19 -0400

    made StratagemCounter

---
+++ StratagemCounter/StratagemCounter.lua
@@ -0,0 +1,80 @@
+_addon.name = 'StratagemCounter'
+_addon.author = 'Lorand'
+_addon.command = 'StratagemCounter'
+_addon.version = '0.1'
+
+require('luau')
+texts = require('texts')
+
+local strat_charge_time = {[1]=240,[2]=120,[3]=80,[4]=60,[5]=48}
+local settings = {pos={x=0,y=0}}
+local stratBox = texts.new(settings)
+
+windower.register_event('load', 'login', function()
+	stratBox = texts.new(settings)
+end)
+
+windower.register_event('prerender', function()
+	player = windower.ffxi.get_player()
+	if player then
+		if S{player.main_job, player.sub_job}:contains('SCH') then
+			local strats = get_available_stratagem_count()
+			local col = '\\cs(0,255,0)'
+			if (strats == 0) then
+				col = '\\cs(255,0,0)'
+			end
+			stratBox:text('Stratagems: '..col..strats..'\\cr')
+			stratBox:visible(true)
+		else
+			stratBox:hide()
+		end
+	else
+		stratBox:hide()
+	end
+end)
+
+--[[
+	Calculates and returns the maximum number of SCH stratagems available for use.
+--]]
+function get_max_stratagem_count()
+	if S{player.main_job, player.sub_job}:contains('SCH') then
+		local lvl = (player.main_job == 'SCH') and player.main_job_level or player.sub_job_level
+		return math.floor(((lvl  - 10) / 20) + 1)
+	else
+		return 0
+	end
+end
+
+--[[
+	Calculates the number of SCH stratagems that are currently available for use. Calculated from the combined recast timer for stratagems and the maximum number
+	of stratagems that are available.  The recast time for each stratagem charge corresponds directly with the maximum number of stratagems that can be used.
+--]]
+function get_available_stratagem_count()
+	local recastTime = windower.ffxi.get_ability_recasts()[231] or 0
+	local maxStrats = get_max_stratagem_count()
+	if (maxStrats == 0) then return 0 end
+	local stratsUsed = (recastTime/strat_charge_time[maxStrats]):ceil()
+	return maxStrats - stratsUsed
+end
+
+--======================================================================================================================
+--[[
+Copyright © 2015, Lorand
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+following conditions are met:
+	* Redistributions of source code must retain the above copyright notice, this list of conditions and the
+	  following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+	  following disclaimer in the documentation and/or other materials provided with the distribution.
+	* Neither the name of StratagemCounter nor the names of its contributors may be used to endorse or promote
+	  products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Lorand BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+--]]
+--======================================================================================================================
