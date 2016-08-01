d7b1cf30f145f59cbd87b99313e8baaa9ca60845
lorand-ffxi <lorand.ffxi@gmail.com>
2016-07-31 22:21:09 -0400

    some config updates; massive overhaul for AutoWS

--- eventScripts/eventScripts.lua
+++ eventScripts/eventScripts.lua
@@ -1,104 +1,112 @@
 _addon.name = 'eventScripts'
 _addon.author = 'Lorand'
 _addon.commands = {'eventScripts', 'escripts'}
-_addon.version = '0.3'
-_addon.lastUpdate = '2015.01.31'
+_addon.version = '0.3.1'
+_addon.lastUpdate = '2016.06.26'
 
 --[[
-	EventScripts is a Windower addon for FFXI that is designed to provide both
-	general and character-specific logon script functionality.  Additionally, it
-	allows some level of event-based execution of commands like the old plugin
-	AutoExec.  The included example of this feature is to send a party invite to
-	a player after receiving a specific message in a tell from them.
+    EventScripts is a Windower addon for FFXI that is designed to provide both
+    general and character-specific logon script functionality.  Additionally, it
+    allows some level of event-based execution of commands like the old plugin
+    AutoExec.  The included example of this feature is to send a party invite to
+    a player after receiving a specific message in a tell from them.
 --]]
 
-config = require('config')
+local config = require('config')
 
-local default_settings = {}
-default_settings.login_cmds = {'wait 5; input /chatmode party'}
-default_settings.addons = {['info']=true,['infoboxes']=true,['jponry']=true}
-default_settings.reactions = {[3] = {['invitemenow_please'] = '/pcmd add {sender}'}}
+local default_settings = {
+    ['login_cmds'] = {'wait 5; input /chatmode party'},
+    ['addons'] = {['info']=true,['infoboxes']=true,['jponry']=true},
+    ['reactions'] = {[3] = {['invitemenow_please'] = '/pcmd add {sender}'}}
+}
 local settings = config.load(default_settings)
-
 local pname = nil
+local loaded_addons = {}
 
 windower.register_event('chat message', function(message, sender, mode, gm)
-	local lmessage = message:lower()
-	local reactions = settings.reactions[mode]
-	if reactions == nil then return end
-	local modMessage = lmessage:gsub(' ', '_')
-	local reaction = reactions[modMessage]
-	if reaction ~= nil then
-		windower.send_command('input '..reaction:gsub('{sender}', sender))
-	elseif lmessage:contains('please') then
-		local words = modMessage:split('_')
-		
-	end
+    local lmessage = message:lower()
+    local reactions = settings.reactions[mode]
+    if reactions == nil then return end
+    local modMessage = lmessage:gsub(' ', '_')
+    local reaction = reactions[modMessage]
+    if reaction ~= nil then
+        windower.send_command('input '..reaction:gsub('{sender}', sender))
+    elseif lmessage:contains('please') then
+        local words = modMessage:split('_')
+    end
 end)
 
 windower.register_event('addon command', function(command,...)
-	if pname == nil then return end
+    if pname == nil then return end
     command = command and command:lower() or 'help'
     local args = {...}
-	
-	if command == 'reload' then
-		windower.send_command('lua reload '.._addon.name)
-	elseif command == 'unload' then
-		windower.send_command('lua unload '.._addon.name)
-	elseif command == 'addon' then
-		changeAddons(args)
-	else
-		addonPrint('Error: Unknown command')
-	end
-	
-	settings:save('all')
+    
+    if command == 'reload' then
+        windower.send_command('lua reload '.._addon.name)
+    elseif command == 'unload' then
+        windower.send_command('lua unload '.._addon.name)
+    elseif command == 'addon' then
+        changeAddons(args)
+    else
+        addonPrint('Error: Unknown command')
+    end
 end)
 
 function changeAddons(args)
-	local cmd = args[1]:lower()
-	local addon = args[2]:lower()
-	
-	if cmd == 'add' then
-		settings[pname].addons[addon] = true
-	elseif cmd == 'remove' then
-		settings[pname].addons[addon] = false
-	else
-		settings[pname].addons[cmd] = true
-	end
+    local cmd = args[1]:lower()
+    local addon = args[2]:lower()
+
+    if cmd == 'add' then
+        settings[pname].addons[addon] = true
+    elseif cmd == 'remove' then
+        settings[pname].addons[addon] = false
+    else
+        settings[pname].addons[cmd] = true
+    end
+    settings:save('all')
 end
 
 windower.register_event('login', 'load', function()
-	local player = windower.ffxi.get_player()
-	if player == nil then return end
-	pname = player.name:lower()
+    local player = windower.ffxi.get_player()
+    if player == nil then return end
+    pname = player.name:lower()
 
-	if not settings[pname] then
-		settings[pname] = {login_cmds = {}, addons = {}, reactions = {}}
-	end
-	
-	local login_cmds = merge(settings.login_cmds, settings[pname].login_cmds)
-	for _,cmd in pairs(login_cmds) do
-		windower.send_command(cmd)
-	end
-	
-	local addons = merge(settings.addons, settings[pname].addons)
-	for addon,use in pairs(addons) do
-		local l = use and 'load ' or 'unload '
-		windower.send_command('lua '..l..addon)
-	end
+    if not settings[pname] then
+        settings[pname] = {login_cmds = {}, addons = {}, reactions = {}}
+    end
+    
+    local login_cmds = merge(settings.login_cmds, settings[pname].login_cmds)
+    for _,cmd in pairs(login_cmds) do
+        windower.send_command(cmd)
+    end
+    
+    local addons = merge(settings.addons, settings[pname].addons)
+    for addon, should_load in pairs(addons) do
+        if should_load then
+            if (loaded_addons[addon] == nil) or (not loaded_addons[addon]) then
+                windower.send_command('lua load '..addon)
+                loaded_addons[addon] = true
+            end
+        else
+            if (loaded_addons[addon] == nil) or loaded_addons[addon] then
+                windower.send_command('lua unload '..addon)
+                loaded_addons[addon] = false
+            end
+        end
+    end
 end)
 
 windower.register_event('logout', function()
-	pname = nil
+    pname = nil
 end)
 
 function merge(...)
-	local args = {...}
-	local newtab = {}
-	for _,t in pairs(args) do
-		for k,v in pairs(t) do newtab[k] = v end
-	end
-	return newtab
+    local args = {...}
+    local newtab = {}
+    for _,t in pairs(args) do
+        for k,v in pairs(t) do newtab[k] = v end
+    end
+    return newtab
 end
 
 -----------------------------------------------------------------------------------------------------------
